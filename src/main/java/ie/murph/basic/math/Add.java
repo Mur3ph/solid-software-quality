cabc527845ab03e06cfd8e8f985f2ea24454c086
Mur3ph <paulmurphymainstreet@gmail.com>
2018-04-04 14:06:30 +0100

    test unit case setup for basic add function

---
+++ src/main/java/ie/murph/basic/math/Add.java
@@ -0,0 +1,11 @@
+package main.java.ie.murph.basic.math;
+
+public class Add
+{
+
+	public int integer(int numberOne, int numberTwo)
+	{
+		return numberOne + numberTwo;
+	}
+	
+}
---
+++ src/test/java/ie/murph/basic/math/AddTest.java
@@ -0,0 +1,28 @@
+package test.java.ie.murph.basic.math;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.ie.murph.basic.math.Add;
+
+public class AddTest
+{
+
+	private Add add;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		add = new Add();
+	}
+
+	@Test
+	public void testAdd()
+	{
+		int testResult = 2 + 2;
+		assertEquals(testResult, add.integer(2, 2));
+	}
+
+}
