f4d7511818512aad54efd69d44473d39d7da8b10
Mur3ph <paulmurphymainstreet@gmail.com>
2018-04-05 11:28:37 +0100

    testing is number even method

--- src/test/java/ie/murph/modulus/ModulusTest.java
+++ src/test/java/ie/murph/modulus/ModulusTest.java
@@ -29,6 +29,23 @@ public class ModulusTest
 		System.out.println("Test Result: " + lastDigitTest + "\n" + "Method Result:" + modulus.getLastDigit(12345));
 	}
 	
-
+	@Test
+	public void isNumberEvenTest()
+	{
+		int evenNumber = 8;
+		int oddNumber = 7;
+		
+		boolean ifEvenNumberTrue = true;
+		boolean ifOddNumberFalse = false;
+		
+		boolean isNumberEven1 = evenNumber % 2 == 0;
+		boolean isNumberEven2 = oddNumber % 2 == 0;
+		
+		assertEquals(ifEvenNumberTrue, modulus.isNumberEven(evenNumber));
+		assertEquals(ifOddNumberFalse, modulus.isNumberEven(oddNumber));
+		
+		assertEquals(isNumberEven1, modulus.isNumberEven(evenNumber));
+		assertEquals(isNumberEven2, modulus.isNumberEven(oddNumber));
+	}
 	
 }
