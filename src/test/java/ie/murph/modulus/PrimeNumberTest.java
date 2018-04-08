76fc4a7ba7ffa4be73c0b57ffd80f500bdba5a63
Mur3ph <paulmurphymainstreet@gmail.com>
2018-04-08 17:38:01 +0100

    added class to test prme number

---
+++ src/test/java/ie/murph/modulus/PrimeNumberTest.java
@@ -0,0 +1,24 @@
+package test.java.ie.murph.modulus;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.ie.murph.modulus.PrimeNumber;
+
+public class PrimeNumberTest
+{
+	private PrimeNumber primeNumber;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		primeNumber = new PrimeNumber();
+	}
+
+	@Test
+	public void getLastDigitTest()
+	{
+
+	}
+
+}
