8596990c6a311dbdd3411d0ba7159f32fcf1ff59
Mur3ph <paulmurphymainstreet@gmail.com>
2018-04-24 21:28:12 +0100

    clean code, rename method

--- src/main/java/ie/murph/modulus/PrimeNumber.java
+++ src/main/java/ie/murph/modulus/PrimeNumber.java
@@ -2,41 +2,42 @@ package main.java.ie.murph.modulus;
 
 import java.util.stream.IntStream;
 
-public class PrimeNumber
-{
+public class PrimeNumber {
 	public boolean isIntegerAPrimeNumber(int numberToCheck) {
-	    // Two is the only even prime number, multiple of one and itself
-	    if (lessThanTwo(numberToCheck)) return false;
-	    
-	    for(int iteration = 2; iteration <= numberToCheck/2; iteration++) {
-	        if(numberToCheck % iteration == 0) {
-	        	 return false;
-	        }
-	    }
-	    return true;
+		// Two is the only even prime number, multiple of one and itself
+		if (isLessThanTwo(numberToCheck))
+			return false;
+
+		for (int iteration = 2; iteration <= numberToCheck / 2; iteration++) {
+			if (numberToCheck % iteration == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
-    private boolean lessThanTwo(int numberToCheck)
-	{
+
+	// Faster than above method
+	public boolean isIntegerAPrimeNumberFaster(int numberToCheck) {
+		if (isLessThanTwo(numberToCheck))
+			return false;
+		for (int iteration = 2; iteration * iteration <= numberToCheck; iteration++) {
+			if (numberToCheck % iteration == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean isIntegerAPrimeNumberJava8(int numberToCheck) {
+		if (isLessThanTwo(numberToCheck))
+			return false;
+		return IntStream.rangeClosed(2, (int) (Math.sqrt(numberToCheck)))
+				.filter(n -> (n & 0X1) == 0)
+				.allMatch(n -> numberToCheck % n != 0);
+	}
+
+	private boolean isLessThanTwo(int numberToCheck) {
 		return numberToCheck < 2;
 	}
 
-//	Faster than above method
-	public boolean isIntegerAPrimeNumberFaster(int numberToCheck) {
-		if (lessThanTwo(numberToCheck)) return false;
-	    for(int iteration = 2; iteration*iteration <= numberToCheck; iteration++) {
-	        if(numberToCheck % iteration == 0) {
-	            return false;
-	        }
-	    }
-	    return true;
-	}
-	
-	public boolean isIntegerAPrimeNumberJava8(int numberToCheck) {
-		if (lessThanTwo(numberToCheck)) return false;
-	    return IntStream.rangeClosed(2, (int) (Math.sqrt(numberToCheck)))
-	      .filter(n -> (n & 0X1) == 0)
-	      .allMatch(n -> numberToCheck % n != 0);
-	}
-	
 }
--- src/test/java/ie/murph/modulus/PrimeNumberTest.java
+++ src/test/java/ie/murph/modulus/PrimeNumberTest.java
@@ -9,58 +9,56 @@ import org.junit.Test;
 
 import main.java.ie.murph.modulus.PrimeNumber;
 
-public class PrimeNumberTest
-{
+public class PrimeNumberTest {
 	private PrimeNumber primeNumber;
 
 	@Before
-	public void setUp() throws Exception
-	{
+	public void setUp() throws Exception {
 		primeNumber = new PrimeNumber();
 	}
 
 	@Test
-	public void isIntegerAPrimeNumber()
-	{
-		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true, primeNumber.isIntegerAPrimeNumber(2));
-		
-//		Boundary tests..
+	public void isIntegerAPrimeNumber() {
+		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true,
+				primeNumber.isIntegerAPrimeNumber(2));
+
+		// Boundary tests..
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumber(2));
-		
+
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumber(0));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumber(1));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumber(4));
 	}
-	
+
 	@Test
-	public void isIntegerAPrimeNumberFasterTest()
-	{
-		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true, primeNumber.isIntegerAPrimeNumberFaster(2));
-		
-//		Boundary tests..
+	public void isIntegerAPrimeNumberFasterTest() {
+		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true,
+				primeNumber.isIntegerAPrimeNumberFaster(2));
+
+		// Boundary tests..
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(2));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(5));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(7));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(11));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(101));
-		
+
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(1));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(0));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberFaster(4));
 	}
-	
+
 	@Test
-	public void isIntegerAPrimeNumberJava8Test()
-	{
-		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true, primeNumber.isIntegerAPrimeNumberFaster(2));
-		
-//		Boundary tests..
+	public void isIntegerAPrimeNumberJava8Test() {
+		assertEquals("Test will succeed. Two is the only even prime number, multiple of one and itself", true,
+				primeNumber.isIntegerAPrimeNumberFaster(2));
+
+		// Boundary tests..
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(2));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(5));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(7));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(11));
 		assertTrue("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(101));
-		
+
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(1));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(0));
 		assertFalse("This will succeed, boundary tests", primeNumber.isIntegerAPrimeNumberJava8(4));
