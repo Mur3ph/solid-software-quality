32d11771c72cef5a7c01f2934fe1263a8d4b5ca6
Mur3ph <paulmurphymainstreet@gmail.com>
2018-12-29 22:43:08 +0000

    fixed bug in test-ng test case

--- src/test/java/ie/murph/testng/InitialTestNg.java
+++ src/test/java/ie/murph/testng/InitialTestNg.java
@@ -1,27 +1,48 @@
 package test.java.ie.murph.testng;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import test.java.ie.murph.selenium.WebDriver;
+
 public class InitialTestNg {
 
-	public String baseUrl = "http://demo.guru99.com/test/newtours/";
-    String driverPath = "C:\\\\dev\\\\test\\\\gecko-driver\\\\geckodriver.exe";
-    public WebDriver driver ; 
-     
-  @Test
-  public void verifyHomepageTitle() {
-       
-      System.out.println("launching firefox browser"); 
-      System.setProperty("webdriver.firefox.marionette", driverPath);
-      driver = new FirefoxDriver();
-      driver.get(baseUrl);
-      String expectedTitle = "Welcome: Mercury Tours";
-      String actualTitle = driver.getTitle();
-      Assert.assertEquals(actualTitle, expectedTitle);
-      driver.close();
-  }
+	private static String baseUrl = "http://demo.guru99.com/test/newtours/";
+	private static String driverPath = "C:\\\\dev\\\\test\\\\gecko-driver\\\\geckodriver.exe";
+	private static WebDriver driver;
+
+	@Test
+	public void verifyHomepageTitle() {
+		System.out.println("launching firefox browser");
+		
+//		System.setProperty("webdriver.firefox.marionette", driverPath);
+		setGeckoDriver();
+		driver = launchFireFoxBrowserDriver();
+		getBrowserToWait();
+		
+		driver.get(baseUrl);
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+		driver.close();
+	}
+
+	private static void setGeckoDriver() {
+		System.setProperty("webdriver.gecko.driver", "C:\\dev\\test\\gecko-driver\\geckodriver.exe");
+	}
+
+	private static FirefoxDriver launchFireFoxBrowserDriver() {
+		return new FirefoxDriver();
+	}
+
+	private static void getBrowserToWait()
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 
 }
