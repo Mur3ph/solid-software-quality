9f722a42c2ecfce646de0d4cd751b8467f5c7b5b
Mur3ph <paulmurphymainstreet@gmail.com>
2019-02-15 21:49:45 +0000

    logging and security test

--- src/test/java/ie/murph/performance/RunPerformanceTest.java
+++ src/test/java/ie/murph/performance/RunPerformanceTest.java
@@ -1,11 +1,16 @@
 package test.java.ie.murph.performance;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import test.java.ie.murph.performance.strings.StringConcatenationTest;
 
 public class RunPerformanceTest
 {
+	private static final Logger LOGGER = LogManager.getLogger(RunPerformanceTest.class.getName());
 	public static void main(String[] arguments)
 	{
+		LOGGER.info("++main()");
 		StringConcatenationTest.stringPerformanceTest();
 		StringConcatenationTest.stringBuilderPerformanceTest();
 		StringConcatenationTest.stringBufferPerformanceTest();
---
+++ src/test/java/ie/murph/security/RunSecurityTest.java
@@ -0,0 +1,12 @@
+package test.java.ie.murph.security;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class RunSecurityTest {
+	private static final Logger LOGGER = LogManager.getLogger(RunSecurityTest.class.getName());
+	public static void main(String[] args) {
+		LOGGER.info("++main()");
+	}
+
+}
