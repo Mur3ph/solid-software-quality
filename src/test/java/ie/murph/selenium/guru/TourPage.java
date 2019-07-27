b5d2a463c1c396a672e555f953012b20c4e7b657
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-27 22:53:12 +0100

    logging

--- src/test/java/ie/murph/selenium/guru/TourPage.java
+++ src/test/java/ie/murph/selenium/guru/TourPage.java
@@ -12,8 +12,8 @@ public class TourPage {
 	private static final Logger LOGGER = LogManager.getLogger(TourPage.class.getName());
 
 	public void runGetTitleTest(WebDriver driver, String baseUrl, String expectedTitle, String actualTitle) {
+		LOGGER.info("++runGetTitleTest()"); 
 		keepBrowserOpenForNumberOfSeconds(driver);
-
 		directToBaseURLOfToursPage(driver, baseUrl);
 		actualTitle = getTitleValueOfTourPage(driver);
 		assertTitleName(actualTitle, actualTitle);
@@ -21,33 +21,32 @@ public class TourPage {
 	}
 	
 	private void keepBrowserOpenForNumberOfSeconds(WebDriver driver) {
+		LOGGER.info("++keepBrowserOpenForNumberOfSeconds()");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	private void directToBaseURLOfToursPage(WebDriver driver, String baseUrl) {
+		LOGGER.info("++directToBaseURLOfToursPage()");
 //		launch Firefox
 		driver.get(baseUrl);
 	}
 
 	private String getTitleValueOfTourPage(WebDriver driver) {
+		LOGGER.info("++getTitleValueOfTourPage()");
 		return driver.getTitle();
 	}
 
 	private void assertTitleName(String actualTitle, String expectedTitle) {
-		/*
-		 * compare the actual title of the page with the expected one and print the
-		 * result as "Passed" or "Failed"
-		 */
+		LOGGER.info("++assertTitleName()");
 		if (actualTitle.contentEquals(expectedTitle)) {
 			LOGGER.info(ITextConstants.GURU99_TOURS_TEST_PASSED);
-			System.out.println(ITextConstants.GURU99_TOURS_TEST_PASSED);
 		} else {
 			LOGGER.info(ITextConstants.GURU99_TOURS_TEST_FAILED);
-			System.out.println(ITextConstants.GURU99_TOURS_TEST_FAILED);
 		}
 	}
 	
 	private void closeFirefoxBrowser(WebDriver driver) {
+		LOGGER.info("++closeFirefoxBrowser()");
 		driver.close();
 	}
 	
