dd3d16e6cf032c188147283896e189aef2edb5b0
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-28 22:34:49 +0100

    yet more logging..

--- src/test/java/ie/murph/selenium/chrome/page/domain/Ebay.java
+++ src/test/java/ie/murph/selenium/chrome/page/domain/Ebay.java
@@ -1,17 +1,22 @@
 package test.java.ie.murph.selenium.chrome.page.domain;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Ebay {
+	private static final Logger LOGGER = LogManager.getLogger(Ebay.class.getName());
 
 	public void findById(WebDriver driver) {
+		LOGGER.info("++findById()");
 		driver.get("https://www.ebay.ie/"); // https://www.myntra.com/login?referer=https://www.myntra.com/
 		driver.findElement(By.id("gh-ac")).sendKeys("Mobiles");
 		driver.findElement(By.id("gh-btn")).click();
 	}
 	
 	public void findByCSSSelector(WebDriver driver) {
+		LOGGER.info("++findByCSSSelector()");
 		driver.get("https://www.ebay.ie/");
 		driver.findElement(By.cssSelector("#gh-ac")).sendKeys("Metal Detector Headphones");
 		driver.findElement(By.cssSelector("#gh-btn")).click();
--- src/test/java/ie/murph/selenium/chrome/page/domain/Myntra.java
+++ src/test/java/ie/murph/selenium/chrome/page/domain/Myntra.java
@@ -5,12 +5,11 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import test.java.ie.murph.selenium.chrome.RunSeleniumChromeTest;
-
 public class Myntra {
-	private static final Logger LOGGER = LogManager.getLogger(RunSeleniumChromeTest.class.getName());
+	private static final Logger LOGGER = LogManager.getLogger(Myntra.class.getName());
 
 	public void findByName(WebDriver driver) throws InterruptedException {
+		LOGGER.info("++findByName()");
 		driver.get("https://www.myntra.com/");
 		driver.findElement(By.name("email")).sendKeys("edureka@gmail.com");
 		Thread.sleep(3000);
@@ -18,6 +17,7 @@ public class Myntra {
 	}
 	
 	public void findByXPath(WebDriver driver) {
+		LOGGER.info("++findByXPath()");
 		driver.get("https://www.myntra.com/login?referer=https://www.myntra.com/");
 		driver.findElement(By.xpath("//input[@placeholder='Your Email Address']")).sendKeys("edureka@yahoo.com");
 	}
