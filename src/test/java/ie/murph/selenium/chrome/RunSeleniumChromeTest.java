ae564e9d626b0742f99f4949604599a6fcdf2210
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-28 15:29:15 +0100

    added browser resizing, waits and delays

--- src/test/java/ie/murph/selenium/chrome/RunSeleniumChromeTest.java
+++ src/test/java/ie/murph/selenium/chrome/RunSeleniumChromeTest.java
@@ -1,5 +1,7 @@
 package test.java.ie.murph.selenium.chrome;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
@@ -18,9 +20,16 @@ private static final Logger LOGGER = LogManager.getLogger(RunSeleniumChromeTest.
 		EChromeDriver.setChromeDriver();
 		WebDriver driver = EChromeDriver.launchChromeBrowserDriver();
 		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
 		driver.get("https://www.ebay.ie/");
 		driver.findElement(By.id("gh-ac")).sendKeys("Mobiles");
 		driver.findElement(By.id("gh-btn")).click();
+		
+		driver.close();
 	}
 	
 }
