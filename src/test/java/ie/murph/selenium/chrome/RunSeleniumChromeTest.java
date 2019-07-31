c09eb61429ed0b8f6f9191c1aad1f9faafdcd882
Mur3ph <paulmurphymainstreet@gmail.com>
2019-07-31 22:16:39 +0100

    added scanner to allow different test to run or numerous times

--- src/test/java/ie/murph/selenium/chrome/RunSeleniumChromeTest.java
+++ src/test/java/ie/murph/selenium/chrome/RunSeleniumChromeTest.java
@@ -1,5 +1,6 @@
 package test.java.ie.murph.selenium.chrome;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.LogManager;
@@ -12,25 +13,27 @@ import test.java.ie.murph.selenium.driver.singleton.EChromeDriver;
 
 public class RunSeleniumChromeTest {
 
-private static final Logger LOGGER = LogManager.getLogger(RunSeleniumChromeTest.class.getName());
-	
-	public RunSeleniumChromeTest() {}
-	
+	private static final Logger LOGGER = LogManager.getLogger(RunSeleniumChromeTest.class.getName());
+	private static final Scanner SCANNER = new Scanner(System.in);
+
+	public RunSeleniumChromeTest() {
+	}
+
 	public static void main(String[] args) throws InterruptedException {
-		LOGGER.info("++main() thread"); 
+		LOGGER.info("++main() thread");
 		EChromeDriver.setChromeDriver();
 		WebDriver driver = EChromeDriver.launchChromeBrowserDriver();
-		
+
 		setupBrowser(driver);
-		
+
 		Ebay ebay = new Ebay();
 		Myntra myntra = new Myntra();
 		ebay.findById(driver);
 		myntra.findByName(driver);
 		ebay.findByCSSSelector(driver);
 		myntra.findByXPath(driver);
-		
-//		driver.close();
+
+		// driver.close();
 	}
 
 	private static void setupBrowser(WebDriver driver) {
@@ -38,6 +41,21 @@ private static final Logger LOGGER = LogManager.getLogger(RunSeleniumChromeTest.
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	// Method to ask the user if they want to try again.
+	public static void askToContinue() throws InterruptedException {
+		System.out.println("Do you want to run test again: (y/n)");
+
+		String s_continue = SCANNER.nextLine();
+		if (s_continue.equalsIgnoreCase("y") || s_continue.equalsIgnoreCase("yes")) {
+			// Reseting and Starting again..
+			main(new String[0]);
+		} else if (s_continue.equalsIgnoreCase("n") || s_continue.equalsIgnoreCase("no")) {
+			System.out.println("Good bye!");
+			System.exit(0);
+		} else
+			askToContinue();
 	}
 
 }
