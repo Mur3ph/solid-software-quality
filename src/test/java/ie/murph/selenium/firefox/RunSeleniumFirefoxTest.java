2f7b9f5a73c587c8b60656b26ccdf2787dd4c888
Mur3ph <paulmurphymainstreet@gmail.com>
2019-08-04 21:18:03 +0100

    clean code firefox driver

--- src/test/java/ie/murph/selenium/firefox/RunSeleniumFirefoxTest.java
+++ src/test/java/ie/murph/selenium/firefox/RunSeleniumFirefoxTest.java
@@ -2,31 +2,28 @@ package test.java.ie.murph.selenium.firefox;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 
-import test.java.ie.murph.selenium.driver.singleton.EFirefoxDriver;
-import test.java.ie.murph.selenium.driver.singleton.EGeckoDriver;
-import test.java.ie.murph.selenium.driver.singleton.EMarionetteDriver;
+import test.java.ie.murph.selenium.driver.BrowserDriver;
 import test.java.ie.murph.selenium.page.domain.TourPage;
+import test.java.ie.murph.selenium.util.Browser;
 import test.java.ie.murph.selenium.util.EURLPathConstants;
 import test.java.ie.murph.selenium.util.ITextConstants;
 
 
 public class RunSeleniumFirefoxTest {
 	private static final Logger LOGGER = LogManager.getLogger(RunSeleniumFirefoxTest.class.getName());
+	private static BrowserDriver browserDriver;
 	
 	public RunSeleniumFirefoxTest() {}
 	
 	public static void main(String[] args) {
 		LOGGER.info("++main() thread"); 
-		EGeckoDriver.setGeckoDriver();
-		EMarionetteDriver.initializeMarionetteDriver();
-		WebDriver driver = EFirefoxDriver.launchFireFoxBrowserDriver();
+		browserDriver = new BrowserDriver(Browser.MOZILLA_FIREFOX);
 
 		String actualTitle = "";
 		
 		TourPage tourPage = new TourPage();
-		tourPage.runGetTitleTest(driver, EURLPathConstants.GURU99_TOURS_BASE_URL.toString(), ITextConstants.GURU99_TOURS_EXPECTED_TITLE, actualTitle);
+		tourPage.runGetTitleTest(browserDriver.getBrowserDriver(), EURLPathConstants.GURU99_TOURS_BASE_URL.toString(), ITextConstants.GURU99_TOURS_EXPECTED_TITLE, actualTitle);
 		LOGGER.info("--main() thread"); 
 	}
 
