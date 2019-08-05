afecee4db260766c8caf7144a737c14890ea27f5
Mur3ph <paulmurphymainstreet@gmail.com>
2019-08-05 22:33:09 +0100

    added logging

--- src/test/java/ie/murph/selenium/internet/explorer/RunInternetExplorerTest.java
+++ src/test/java/ie/murph/selenium/internet/explorer/RunInternetExplorerTest.java
@@ -1,13 +1,17 @@
 package test.java.ie.murph.selenium.internet.explorer;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import test.java.ie.murph.selenium.util.EURLPathConstants;
 
 public class RunInternetExplorerTest {
+	private static final Logger LOGGER = LogManager.getLogger(RunInternetExplorerTest.class.getName());
 
 	public static void main(String[] args) {
+		LOGGER.info("++main() thread" + args.length);
 		System.setProperty(EURLPathConstants.INTERNET_EXPLORER_WEB_DRIVER_PROPERTY.toString(), EURLPathConstants.INTERNET_EXPLORER_WEB_DRIVER_EXE_JAR_PATH.toString());
 
 		WebDriver driver = new InternetExplorerDriver();
