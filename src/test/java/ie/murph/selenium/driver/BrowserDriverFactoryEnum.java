07ed6fda99b2b068cba3122ec6984c7c56077864
Mur3ph <paulmurphymainstreet@gmail.com>
2019-08-10 22:49:20 +0100

    added driver singleton enum factory

---
+++ src/test/java/ie/murph/selenium/driver/BrowserDriverFactoryEnum.java
@@ -0,0 +1,37 @@
+package test.java.ie.murph.selenium.driver;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import test.java.ie.murph.selenium.util.Browser;
+
+public enum BrowserDriverFactoryEnum {
+
+	INSTANCE;
+	private static final Logger LOGGER = LogManager.getLogger(BrowserDriverFactoryEnum.class.getName());
+
+	public synchronized WebDriver createBrowserDriver(Browser browser) {
+
+		LOGGER.info("++createBrowserDriver(" + browser + ")");
+		switch (browser) {
+		case GOOGLE_CHROME:
+			// setDriverSystemPropertyVariable(Browser.GOOGLE_CHROME);
+			// instantiateChromeBrowserDriver();
+			break;
+		case MOZILLA_FIREFOX:
+			// setDriverSystemPropertyVariable(Browser.MOZILLA_FIREFOX);
+			// instantiateFireFoxBrowserDriver();
+			break;
+		case INTERNET_EXPLORER:
+			// setDriverSystemPropertyVariable(Browser.INTERNET_EXPLORER);
+			// instantiateInternetExplorerBrowserDriver();
+			break;
+		default:
+			LOGGER.info("BROWSER NOT SUPPORTED");
+			break;
+		}
+		return null;
+	}
+
+}
