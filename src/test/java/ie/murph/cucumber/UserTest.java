918f6ec5815a88ea482c7b096fc3e09336286ff4
Mur3ph <paulmurphymainstreet@gmail.com>
2019-08-19 23:34:04 +0100

    trying to write cucumber test

---
+++ src/test/java/ie/murph/cucumber/UserTest.java
@@ -0,0 +1,9 @@
+package test.java.ie.murph.cucumber;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class UserTest {
+	
+}
--- src/test/java/ie/murph/cucumber/runner/Runner.java
+++ src/test/java/ie/murph/cucumber/runner/Runner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;		
 
 @RunWith(Cucumber.class)				
-@CucumberOptions(features="Features",glue={"classpath:com/runner", "classpath:com/stepdefinition"})						
+@CucumberOptions(features="Features",glue={"classpath:com/stepdefinition", "classpath:com/runner"})						
 public class Runner 				
 {		
 	
