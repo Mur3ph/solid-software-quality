c5088868e438c27eb66d52de87f1d91415deb14b
Mur3ph <paulmurphymainstreet@gmail.com>
2019-08-29 22:43:40 +0100

    cucumber cucumber cucumber

--- src/test/java/ie/murph/cucumber/UserTest.java
+++ src/test/java/ie/murph/cucumber/UserTest.java
@@ -1,9 +1,14 @@
 package test.java.ie.murph.cucumber;
 
 import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+@SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"})
 public class UserTest {
-	
+//	https://examples.javacodegeeks.com/core-java/junit/junit-cucumber-example/
 }
---
+++ src/test/resources/features/InitialCucumberTest.feature
@@ -0,0 +1,10 @@
+
+Feature: Reset functionality on login page of Application 
+
+Scenario: Verification of Reset button 
+
+Given Open the Firefox and launch the application			
+
+When Enter the Username and Password			
+
+Then Reset the credential			
