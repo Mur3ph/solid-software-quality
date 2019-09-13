a3cc8e74e1511d6b0b99b20abee5589a689c4af5
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-13 23:14:47 +0100

    random performance test

---
+++ src/test/java/ie/murph/performance/random/RandomTest.java
@@ -0,0 +1,20 @@
+package test.java.ie.murph.performance.random;
+
+public class RandomTest {
+	
+	 public static void main(String args[]) {
+	        long start = System.currentTimeMillis();
+	        for (int i = 0; i < 100000000; i++) {
+	            Math.random();
+	        }
+	        System.out.println("Math.random() Time: " + (System.currentTimeMillis() - start) + "ms");
+
+	        start = System.currentTimeMillis();
+	        for (long i = 0; i < 8000000000L; i++) {
+	            // perform random math functions to use CPU
+	            int v = 4 * 67 + 87 / 45 * 2345;
+	        }
+	        System.out.println("Simple Math Time: " + (System.currentTimeMillis() - start) + "ms");
+	    }
+
+}
