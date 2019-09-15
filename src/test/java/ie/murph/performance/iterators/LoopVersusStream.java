f44d2506e3b6c78bd14b5f0abf43571124a6cc6e
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-15 22:38:57 +0100

    JMH (Java Microbenchmark Harness) article

--- src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
+++ src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
@@ -3,6 +3,8 @@ package test.java.ie.murph.performance.iterators;
 import org.openjdk.jmh.annotations.*;
 
 public class LoopVersusStream {
+	
+//	https://dzone.com/articles/java-performance-for-looping-vs-streaming
 
 	private static final int ITERATIONS = 10_000;
 
