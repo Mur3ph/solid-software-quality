012dc6bd6d015ce2d3b902320c329be71ded57ef
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-15 22:00:52 +0100

    tidy random performance test

--- src/test/java/ie/murph/performance/random/RandomTest.java
+++ src/test/java/ie/murph/performance/random/RandomTest.java
@@ -1,20 +1,21 @@
 package test.java.ie.murph.performance.random;
 
 public class RandomTest {
-	
-	 public static void main(String args[]) {
-	        long start = System.currentTimeMillis();
-	        for (int i = 0; i < 100000000; i++) {
-	            Math.random();
-	        }
-	        System.out.println("Math.random() Time: " + (System.currentTimeMillis() - start) + "ms");
 
-	        start = System.currentTimeMillis();
-	        for (long i = 0; i < 8000000000L; i++) {
-	            // perform random math functions to use CPU
-	            int v = 4 * 67 + 87 / 45 * 2345;
-	        }
-	        System.out.println("Simple Math Time: " + (System.currentTimeMillis() - start) + "ms");
-	    }
+	public static void main(String args[]) {
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 100000000; i++) {
+			Math.random();
+		}
+		System.out.println("Math.random() Time: " + (System.currentTimeMillis() - start) + "ms");
+
+		start = System.currentTimeMillis();
+		int simpleMathFunctionPerformanceTest = 0;
+		for (long i = 0; i < 8000000000L; i++) {
+			// perform random math functions to use CPU
+			simpleMathFunctionPerformanceTest = 4 * 67 + 87 / 45 * 2345;
+		}
+		System.out.println("Simple Math Time: " + (System.currentTimeMillis() - start) + "ms. Answer: " + simpleMathFunctionPerformanceTest);
+	}
 
 }
