50c7736e74ccb1797835761a7d440f80aa22f028
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-17 22:40:14 +0100

    testing collection interface Map

---
+++ src/test/java/ie/murph/collections/MapCollection.java
@@ -0,0 +1,33 @@
+package test.java.ie.murph.collections;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapCollection {
+	
+	private Map<Object, Object> map;
+	
+	public void createTreeMap() {
+		map = new TreeMap<Object, Object>();
+	}
+	
+	public void createHashMap() {
+		map = new HashMap<Object, Object>();
+	}
+	
+	public void createLinkedHashMap() {
+		map = new LinkedHashMap<Object, Object>();
+	}
+	
+	public void createHashTable() {
+		map = new Hashtable<Object, Object>();
+	}
+	
+	public Map<Object, Object> getMap(){
+		return map;
+	}
+
+}
