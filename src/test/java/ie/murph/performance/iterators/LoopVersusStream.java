33e44f354d04608223e0bcd41d43f0755d818a33
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-19 22:49:56 +0100

    manually clean .m2 repository.

--- src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
+++ src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
@@ -7,8 +7,13 @@ public class LoopVersusStream {
 //	https://dzone.com/articles/java-performance-for-looping-vs-streaming
 
 	private static final int ITERATIONS = 10_000;
+	
+	public static void main(String[] args) {
+		LoopVersusStream loopVersusStream = new LoopVersusStream();
+		System.out.println(loopVersusStream.forUp());
+	}
 
-	@Benchmark
+//	@Benchmark
 	public int forUp() {
 		int sum = 0;
 		for (int i = 0; i < ITERATIONS; i++) {
