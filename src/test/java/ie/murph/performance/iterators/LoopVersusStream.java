118697ea50dc4ca3b8b480642b36ef13ed2257d0
Mur3ph <paulmurphymainstreet@gmail.com>
2019-09-22 22:27:02 +0100

    benchmark but trying to configure java

--- src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
+++ src/test/java/ie/murph/performance/iterators/LoopVersusStream.java
@@ -1,18 +1,20 @@
 package test.java.ie.murph.performance.iterators;
 
+import java.util.stream.IntStream;
+
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class LoopVersusStream {
-	
-//	https://dzone.com/articles/java-performance-for-looping-vs-streaming
-//	https://www.baeldung.com/java-microbenchmark-harness
+
+	// https://dzone.com/articles/java-performance-for-looping-vs-streaming
+	// https://www.baeldung.com/java-microbenchmark-harness
 
 	private static final int ITERATIONS = 10_000;
-	
-	public static void main(String[] args) {
-		LoopVersusStream loopVersusStream = new LoopVersusStream();
-		System.out.println(loopVersusStream.forUp());
-	}
+
+//	public static void main(String[] args) {
+//		LoopVersusStream loopVersusStream = new LoopVersusStream();
+//		System.out.println(loopVersusStream.forUp());
+//	}
 
 	@Benchmark
 	public int forUp() {
@@ -22,5 +24,27 @@ public class LoopVersusStream {
 		}
 		return sum;
 	}
+
+	@Benchmark
+	public int forDown() {
+		int sum = 0;
+		for (int i = ITERATIONS; i-- > 0;) {
+			sum += i;
+		}
+		return sum;
+	}
+	
+	@Benchmark
+	public int stream() {
+	    return IntStream.range(0, ITERATIONS)
+	        .sum();
+	}
+	
+	@Benchmark
+	public int math() {
+	    return ITERATIONS * (ITERATIONS + 1) / 2;
+	}
+	
+	
 
 }
